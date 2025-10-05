e169678d1bcb935adb445dedaa249501495c45f6
MarkParal <markwparal@gmail.com>
2025-10-05 18:01:14 -0400

    Adjusted tle functions to read entire strings, lines, etc.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ fn main() {
     let tle_string = "ISS (ZARYA)\n1 25544U 98067A   08264.51782528 -.00002182 -00100-2 -11606-4 0  2927\n2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537";
     
     let tle_data = tle::from_string(tle_string);
-    // println!("Satellite catalog number: {}", tle.satellite_catalog_number);
-    // println!("Classification: {}", tle.classification);
-    // println!("International designator: {}", tle.international_designator);
-    // println!("Epoch year: {}", tle.epoch_year);
-    // println!("Epoch day: {}", tle.epoch_day);
-    // println!("First derivative of mean motion: {}", tle.first_derivative_of_mean_motion);
-    // println!("Second derivative of mean motion: {}", tle.second_derivative_of_mean_motion);
-    // println!("Bstar: {}", tle.bstar);
-    // println!("Ephemeris type: {}", tle.ephemeris_type);
-    // println!("Element set number: {}", tle.element_set_number);
-    // println!("Inclination: {}", tle.inclination);
-    // println!("Right ascension of ascending node: {}", tle.right_ascension_of_ascending_node);
-    // println!("Eccentricity: {}", tle.eccentricity);
-    // println!("Argument of perigee: {}", tle.argument_of_perigee);
-    // println!("Mean anomaly: {}", tle.mean_anomaly);
-    // println!("Mean motion: {}", tle.mean_motion);
-    // println!("Revolution number at epoch: {}", tle.revolution_number_at_epoch);
+    println!("Satellite catalog number: {}", tle_data.satellite_catalog_number);
+    println!("Classification: {}", tle_data.classification);
+    println!("International designator: {}", tle_data.international_designator);
+    println!("Epoch year: {}", tle_data.epoch_year);
+    println!("Epoch day: {}", tle_data.epoch_day);
+    println!("First derivative of mean motion: {}", tle_data.first_derivative_of_mean_motion);
+    println!("Second derivative of mean motion: {}", tle_data.second_derivative_of_mean_motion);
+    println!("Bstar: {}", tle_data.bstar);
+    println!("Ephemeris type: {}", tle_data.ephemeris_type);
+    println!("Element set number: {}", tle_data.element_set_number);
+    println!("Inclination: {}", tle_data.inclination);
+    println!("Right ascension of ascending node: {}", tle_data.right_ascension_of_ascending_node);
+    println!("Eccentricity: {}", tle_data.eccentricity);
+    println!("Argument of perigee: {}", tle_data.argument_of_perigee);
+    println!("Mean anomaly: {}", tle_data.mean_anomaly);
+    println!("Mean motion: {}", tle_data.mean_motion);
+    println!("Revolution number at epoch: {}", tle_data.revolution_number_at_epoch);
 }
--- src/tle.rs
+++ src/tle.rs
@@ -1,29 +1,88 @@
-pub struct Tle {
-    common_name: String,
-    satellite_catalog_number: i32,
-    classification: char,
-    international_designator: String,
-    epoch_year: i32,
-    epoch_day: f64,
-    first_derivative_of_mean_motion: f64,
-    second_derivative_of_mean_motion: f64,
-    bstar: f64,
-    ephemeris_type: i32,
-    element_set_number: i32,
-    inclination: f64,
-    right_ascension_of_ascending_node: f64,
-    eccentricity: f64,
-    argument_of_perigee: f64,
-    mean_anomaly: f64,
-    mean_motion: f64,
-    revolution_number_at_epoch: i64,
+pub struct TLE {
+    pub common_name: String,
+    pub satellite_catalog_number: i32,
+    pub classification: char,
+    pub international_designator: String,
+    pub epoch_year: i32,
+    pub epoch_day: f64,
+    pub first_derivative_of_mean_motion: f64,
+    pub second_derivative_of_mean_motion: f64,
+    pub bstar: f64,
+    pub ephemeris_type: i32,
+    pub element_set_number: i32,
+    pub inclination: f64,
+    pub right_ascension_of_ascending_node: f64,
+    pub eccentricity: f64,
+    pub argument_of_perigee: f64,
+    pub mean_anomaly: f64,
+    pub mean_motion: f64,
+    pub revolution_number_at_epoch: i64,
 }
 
-pub fn from_string(tle_string: &str) {
+pub fn from_string(tle_string: &str) -> TLE {
     /// Function to parse the TLE string and return a Tle struct
+    
+    // Split the TLE string into lines
+    let lines = tle_string.split("\n").collect::<Vec<&str>>();
 
-    // Create a mutable Tle struct
-    let mut tle = Tle {
+    // TLEs can be 2 or 3 lines, account for both cases
+    if lines.len() == 3 {
+        // Create TLE struct
+        let mut tle = from_lines(lines[1],lines[2]);
+
+        // Extract the common name of the satellite from line 0
+        if lines[0].len() < 1 || lines[0].len() > 24 {
+            println!("TLE string for Line 0 is invalid");
+        } else {
+            tle.common_name = lines[0].to_string();
+        }
+
+        // Return TLE struct
+        return tle;
+
+    } else if lines.len() == 2 {
+        // Create TLE struct
+        let tle = from_lines(lines[0],lines[1]);
+
+        // Return TLE struct
+        return tle;
+    
+    } else {
+        println!("TLE string is invalid");
+
+        // Make an empty TLE struct
+        let tle = TLE {
+            common_name: String::new(),
+            satellite_catalog_number: 0,
+            classification: '0',
+            international_designator: String::new(),
+            epoch_year: 0,
+            epoch_day: 0.0,
+            first_derivative_of_mean_motion: 0.0,
+            second_derivative_of_mean_motion: 0.0,
+            bstar: 0.0,
+            ephemeris_type: 0,
+            element_set_number: 0,
+            inclination: 0.0,
+            right_ascension_of_ascending_node: 0.0,
+            eccentricity: 0.0,
+            argument_of_perigee: 0.0,
+            mean_anomaly: 0.0,
+            mean_motion: 0.0,
+            revolution_number_at_epoch: 0,
+        };
+
+        // Return TLE struct
+        return tle;
+    }
+    
+}
+
+pub fn from_lines(line1: &str, line2: &str) -> TLE {
+    /// Function to parse the TLE lines into a TLE struct
+
+    // Create mutable TLE struct
+    let mut tle = TLE {
         common_name: String::new(),
         satellite_catalog_number: 0,
         classification: '0',
@@ -44,95 +103,85 @@ pub fn from_string(tle_string: &str) {
         revolution_number_at_epoch: 0,
     };
     
-    // Split the TLE string into lines
-    let lines = tle_string.split("\n").collect::<Vec<&str>>();
-
-    // TLEs can be 2 or 3 lines, account for both cases
-    if lines.len() == 3 {
-        // First line is the name of the satellite
-        if lines[0].len() < 1 || lines[0].len() > 24 {
-            println!("TLE string is invalid");
-        } else {
-            tle.common_name = lines[0].to_string();
-        }
-
-        // Second line is the satellite catalog number, classification, international designator, epoch year, epoch day, first derivative of mean motion, second derivative of mean motion, bstar, ephemeris type, element set number
-        if lines[1].len() < 69 || lines[1].len() > 69 {
-            println!("TLE string is invalid");
-        } else {
-            // Line 1
-            // Satellite catalog number
-            tle.satellite_catalog_number = lines[1][2..7].parse::<i32>().unwrap();
-
-            // Classification
-            tle.classification = lines[1][7..8].parse::<char>().unwrap();
-
-            // International designator
-            tle.international_designator = lines[1][9..17].to_string();
-
-            // Epoch year (last two numbers)
-            tle.epoch_year = lines[1][18..20].parse::<i32>().unwrap();
-
-            // Epoch day of year
-            tle.epoch_day = lines[1][20..32].parse::<f64>().unwrap();
-
-            // 1st derivative of mean motion [revs/day^2]
-            tle.first_derivative_of_mean_motion = lines[1][33..43].parse::<f64>().unwrap() * 2.0;
-
-            // 2nd derivative of mean motion [revs/days^3]
-            // Account for - in 2nd derivative of mean motion
-            if lines[1][44..45].parse::<char>().unwrap() == '-' {
-                tle.second_derivative_of_mean_motion = format!("-0.{}", lines[1][45..50].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(lines[1][50..52].parse::<i32>().unwrap()) * 6.0_f64; // TODO: Fix floating point precision error
-            } else {
-                tle.second_derivative_of_mean_motion = format!("0.{}", lines[1][45..50].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(lines[1][50..52].parse::<i32>().unwrap()) * 6.0_f64; // TODO: Fix floating point precision error
-            }
-
-            // B* [1/Earth Radii]
-            // Account for - in B* term
-            if lines[1][53..54].parse::<char>().unwrap() == '-' {
-                tle.bstar = format!("-0.{}", lines[1][54..59].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(lines[1][59..61].parse::<i32>().unwrap()); // TODO: Fix floating point precision error
-            } else {
-                tle.bstar = format!("0.{}", lines[1][54..59].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(lines[1][59..61].parse::<i32>().unwrap()); // TODO: Fix floating point precision error
-            }
-
-            // Ephemeris type
-            tle.ephemeris_type = lines[1][62..63].parse::<i32>().unwrap();
-
-            // Element set number
-            tle.element_set_number = lines[1][64..68].trim().parse::<i32>().unwrap();
-        }
-
-        if lines[2].len() < 69 || lines[2].len() > 69 {
-            println!("TLE string is invalid");
-        } else {
-            // Line 2
-            // Inclination [degs]
-            tle.inclination = lines[2][8..16].trim().parse::<f64>().unwrap();
-
-            // Right ascension of ascending node [degs]
-            tle.right_ascension_of_ascending_node = lines[2][17..25].trim().parse::<f64>().unwrap();
-
-            // Eccentricity
-            tle.eccentricity = format!("0.{}", lines[2][26..33].trim()).parse::<f64>().unwrap();
-
-            // Argument of perigee [degs]
-            tle.argument_of_perigee = lines[2][34..42].trim().parse::<f64>().unwrap();
-
-            // Mean anomaly [degs]
-            tle.mean_anomaly = lines[2][43..51].trim().parse::<f64>().unwrap();
-
-            // Mean motion [revs/day]
-            tle.mean_motion = lines[2][52..63].trim().parse::<f64>().unwrap();
-
-            // Revolution number at epoch
-            tle.revolution_number_at_epoch = lines[2][63..68].trim().parse::<i64>().unwrap();
-            // TODO: Continue...
-            println!("{}", tle.revolution_number_at_epoch);
-        }
-    } else if lines.len() == 2 {
+    // Parse through line 1 and populate TLE struct
+    if line1.len() < 69 || line1.len() > 69 {
         println!("TLE string is invalid");
     } else {
-        println!("TLE string is invalid");
+        // Line 1
+        // Satellite catalog number
+        tle.satellite_catalog_number = line1[2..7].parse::<i32>().unwrap();
+
+        // Classification
+        tle.classification = line1[7..8].parse::<char>().unwrap();
+
+        // International designator
+        tle.international_designator = line1[9..17].to_string();
+
+        // Epoch year (last two numbers)
+        tle.epoch_year = line1[18..20].parse::<i32>().unwrap();
+
+        // Epoch day of year
+        tle.epoch_day = line1[20..32].parse::<f64>().unwrap();
+
+        // 1st derivative of mean motion [revs/day^2]
+        tle.first_derivative_of_mean_motion = line1[33..43].parse::<f64>().unwrap() * 2.0;
+
+        // 2nd derivative of mean motion [revs/days^3]
+        // Account for - in 2nd derivative of mean motion
+        if line1[44..45].parse::<char>().unwrap() == '-' {
+            tle.second_derivative_of_mean_motion = format!("-0.{}", line1[45..50].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(line1[50..52].parse::<i32>().unwrap()) * 6.0_f64; // TODO: Fix floating point precision error
+        } else {
+            tle.second_derivative_of_mean_motion = format!("0.{}", line1[45..50].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(line1[50..52].parse::<i32>().unwrap()) * 6.0_f64; // TODO: Fix floating point precision error
+        }
+
+        // B* [1/Earth Radii]
+        // Account for - in B* term
+        if line1[53..54].parse::<char>().unwrap() == '-' {
+            tle.bstar = format!("-0.{}", line1[54..59].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(line1[59..61].parse::<i32>().unwrap());
+        } else {
+            tle.bstar = format!("0.{}", line1[54..59].trim()).parse::<f64>().unwrap() * 10.0_f64.powi(line1[59..61].parse::<i32>().unwrap());
+        }
+
+        // Ephemeris type
+        tle.ephemeris_type = line1[62..63].parse::<i32>().unwrap();
+
+        // Element set number
+        tle.element_set_number = line1[64..68].trim().parse::<i32>().unwrap();
     }
-    
+
+    // Parse through line 2 and populate TLE struct
+    if line2.len() < 69 || line2.len() > 69 {
+        println!("TLE string is invalid");
+    } else {
+        // Line 2
+        // Inclination [degs]
+        tle.inclination = line2[8..16].trim().parse::<f64>().unwrap();
+
+        // Right ascension of ascending node [degs]
+        tle.right_ascension_of_ascending_node = line2[17..25].trim().parse::<f64>().unwrap();
+
+        // Eccentricity
+        tle.eccentricity = format!("0.{}", line2[26..33].trim()).parse::<f64>().unwrap();
+
+        // Argument of perigee [degs]
+        tle.argument_of_perigee = line2[34..42].trim().parse::<f64>().unwrap();
+
+        // Mean anomaly [degs]
+        tle.mean_anomaly = line2[43..51].trim().parse::<f64>().unwrap();
+
+        // Mean motion [revs/day]
+        tle.mean_motion = line2[52..63].trim().parse::<f64>().unwrap();
+
+        // Revolution number at epoch
+        tle.revolution_number_at_epoch = line2[63..68].trim().parse::<i64>().unwrap();
+    }
+
+    return tle;
+}
+
+pub fn tle_checksum() -> bool {
+    /// Validate the TLE hasn't been corrupted by running a checksum test
+
+    // TODO: Write this function!!!
+    return true;
 }
